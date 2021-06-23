53892fde558b78bc062cf3c55e23d4f25e041c46
ElenaVedernikova <lenok-v@live.ru>
2021-06-23 18:49:16 +0300

    gitignore

---
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    public void shouldHaveAllMethodsFromSuper() {
+        Book book = new Book();
+
+    }
+}
